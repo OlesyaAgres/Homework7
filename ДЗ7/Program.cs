a164241722e4e9afac0edb949335c0cf171c5ff5
Olesya <olesyaspb.ru@mail.ru>
2023-05-03 11:42:04 +0300

    написаны решения

---
+++ ДЗ7/Program.cs
@@ -0,0 +1,175 @@
+﻿// Практическое задание 7 
+
+//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+double[,] Gen2DArray(int row, int column)
+{
+    int i = 0; int j = 0;
+    double[,] arr = new double[row, column];
+    while (i < row)
+    {
+        j = 0;
+        while (j < column)
+        {
+            arr[i, j] = Math.Round(new Random().Next(0, 101) + new Random().NextDouble(), 4);
+            j++;
+        }
+        i++;
+    }
+    return arr;
+}
+void Print2DArr(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = (ConsoleColor)(new System.Random().Next(0,16));
+            Console.Write(arr[i, j] + "\t");
+            Console.ResetColor(); 
+        }
+        Console.WriteLine("");
+    }
+}
+int n = ReadData("Задайте количество строк двумерного массива: ");
+int m = ReadData("Задайте количество столбцов двумерного массива: ");
+
+double[,] arr2D = Gen2DArray(n, m);
+
+Print2DArr(arr2D);
+*/
+
+//2 вариант написания
+/*
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
+*/
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+/*
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5; 
+int m = 7; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
+*/
+
+
+//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+/*
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+int[,] Gen2DArrayInt(int row, int column)
+{
+    int i = 0; int j = 0;
+    int[,] arr = new int[row, column];
+    while (i < row)
+    {
+        j = 0;
+        while (j < column)
+        {
+            arr[i, j] = new Random().Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+             Console.Write(arr[i, j] + "\t");
+         }
+        Console.WriteLine("");
+    }
+}
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int[] MeanColum(int[,] arr)
+{
+    int[] means = new int[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            means[i] = means[i] + arr[j, i];
+        }
+        means[i] = means[i] / arr.GetLength(0);
+    }
+    return means;
+}
+int row = ReadData("Количество строк матрицы: ");
+int column = ReadData("Количество столбцов матрицы: ");
+
+int[,] arr2D = Gen2DArrayInt(row, column);
+
+int[] average = MeanColum(arr2D);
+
+Print2DArr(arr2D);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое по столбцам: ");
+
+Print1DArr(average);
+*/
